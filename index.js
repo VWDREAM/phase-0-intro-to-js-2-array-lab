5866b971eb00f293e067ea4c894963eadb38bf02
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-02-27 16:48:50 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function appendCat(name){
+    const newCatsArray= [...cats, name];
+    return newCatsArray;
+}
+function prependCat(name){
+    const newCatsArray=[name, ...cats];
+    return newCatsArray;
+}
+function removeLastCat(name){
+    const newCatsArray = cats.slice(0, cats.length - 1);
+    return newCatsArray;
+}
+function removeFirstCat(name){
+    const newCatsArray = cats.slice(1);
+    return newCatsArray;
+}
